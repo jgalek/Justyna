dbedbbfebd1acb8b6da4e143bd9e0806dba7da4d
ketevank <p.glogowska20@gmail.com>
2019-03-07 19:04:08 +0100

    Initial commit

---
+++ app/src/Repository/TestRepository.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Test repository.
+ */
+
+namespace App\Repository;
+
+/**
+ * Class TestRepository.
+ */
+class TestRepository
+{
+    /**
+     * Data.
+     *
+     * @var array
+     */
+    private $data = [
+        [
+            'title' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'tags' => [
+                'Sed',
+                'convallis',
+                'nibh',
+            ],
+        ],
+        [
+            'title' => 'Etiam diam ipsum, dignissim eget suscipit nec, faucibus accumsan felis',
+            'tags' => [
+                'Phasellus',
+                'vestibulum',
+                'tortor',
+            ],
+        ],
+        [
+            'title' => 'Nullam eget dui blandit, scelerisque lacus a, sagittis nibh',
+            'tags' => [
+                'Curabitur',
+                'consectetur',
+                'porttitor',
+            ],
+        ],
+    ];
+
+    /**
+     * Find all.
+     *
+     * @return array Result
+     */
+    public function findAll(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Find one by Id.
+     *
+     * @param int $id Id
+     *
+     * @return array|null Result
+     */
+    public function findById(int $id): ?array
+    {
+        return isset($this->data[$id]) && count($this->data)
+            ? $this->data[$id] : null;
+    }
+}
